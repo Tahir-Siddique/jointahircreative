9c882e4fa991c5535c6722f6f533d06607406e8c
Tahir-Siddique <tahircreativedev577@gmail.com>
2022-01-29 20:57:17 +0000

    Server Commit

--- tahirccreative/settings.py
+++ tahirccreative/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/jointahirsiddique/.virtualenvs/venv/lib/python3.9/dist-packages/django/contrib/admin/static/admin/'
