ef8e5626c55602a205c83c5fe134e7db85bcccec
= <=>
2022-02-10 16:54:10 +0500

    Error

--- tahirccreative/urls.py
+++ tahirccreative/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', admin.site.urls),
-    path('admin/', admin.site.urls),
+    path('', include("tahircreativecustomers.urls")),
     path('authenticatecustomer/', include("tahircreativecustomers.urls")),
+    path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
