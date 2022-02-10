dc73546431b8c6929c3d53127a6e769a9f7aa439
= <=>
2022-02-10 16:56:36 +0500

    Admin Support

--- tahirccreative/urls.py
+++ tahirccreative/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('authenticatecustomer/', include("tahircreativecustomers.urls")),
     path('admin/', admin.site.urls),
-    path('', include("tahircreativecustomers.urls")),
+    path('',  admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
