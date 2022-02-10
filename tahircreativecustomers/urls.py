0e80342e167575d2e76159b87bc0c2b2de6dc8f4
= <=>
2022-02-10 17:00:58 +0500

    Updating

--- tahircreativecustomers/urls.py
+++ tahircreativecustomers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import isUserBlocked,error,project_details
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', error,name="error"),
     path('projects/<slug>', project_details,name="details"),
     path('<str:key>', isUserBlocked,name="isUserBlocked"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
